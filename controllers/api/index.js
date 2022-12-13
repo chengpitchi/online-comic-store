d6c9e642e629cc9d5e215bf491e490715a5f46c0
CheBar1 <chebarclay@live.com.au>
2022-12-13 20:05:21 +1030

    add orderRoute

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const productRoutes = require('./productRoutes');
 const reviewRoutes = require('./reviewRoutes'); 
 const userRoutes = require('./userRoutes'); 
+const orderRoutes = require('./orderRoutes');
 
 router.use('/products', productRoutes);
 router.use('/reviews', reviewRoutes); 
 router.use('/users', userRoutes); 
+router.use('/orders', orderRoutes);
 
 module.exports = router;
