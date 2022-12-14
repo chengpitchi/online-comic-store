328b72b8a9ca47effd39df9f4ae33e0f345ce45e
Billy Cheng <chengpitchi@yahoo.com>
2022-12-15 02:13:17 +1030

    add order item routes

---
+++ controllers/api/orderItemRoutes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection'); 
+const { OrderItem } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+      const orderItemData = await OrderItem.create(req.body);
+      res.status(200).json(orderItemData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+router.put('/:id', async (req, res) => {
+// update a orderItem by its `id` value
+try {
+    const orderItemData = await OrderItem.update(req.body, {
+    where: {
+        id: req.params.id,
+    },
+    });
+
+    if (!orderItemData) {
+    res.status(404).json({ message: 'No order Item found with that id!' });
+    return;
+    }
+
+    res.status(200).json(orderItemData);
+} catch (err) {
+    res.status(500).json(err);
+}
+});
+
+router.delete('/:id', async (req, res) => {
+// delete a order item by its `id` value
+try {
+    const orderItemData = await OrderItem.destroy({
+    where: {
+        id: req.params.id,
+    },
+    });
+
+    if (!orderItemData) {
+    res.status(404).json({ message: 'No order Item found with that id!' });
+    return;
+    }
+
+    res.status(200).json(orderItemData);
+} catch (err) {
+    res.status(500).json(err);
+}
+});
+  
+module.exports = router;
