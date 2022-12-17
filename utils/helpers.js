91f1f7e8bc6569e7bfa8ce3a5f56d3faaefec211
Billy Cheng <chengpitchi@yahoo.com>
2022-12-17 18:50:38 +1030

    bug fix for heroku

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,17 +1,17 @@
 module.exports = {
-    // The custom helper 'format_datetime' takes in a timestamp
-    format_datetime: (date) => {
-      return `${new Date(date).getFullYear()}-${new Date(date).getMonth() + 1}-${new Date(date).getDate()} ${date.toLocaleTimeString()}`;
-    },
-    net_amount: (price) => {
-      price = price - (Math.round(price * 0.1 * 100) / 100).toFixed(2); 
-      return (Math.round(price * 100) / 100).toFixed(2);
-    },
-    gst_amount: (price) => {
-      price = price * 0.1; 
-      return (Math.round(price * 100) / 100).toFixed(2);
-    },
-    round: (price) => {
-      return price.toFixed(2);
-    }
-  };
+  // The custom helper 'format_datetime' takes in a timestamp
+  format_datetime: (date) => {
+    return `${new Date(date).getFullYear()}-${new Date(date).getMonth() + 1}-${new Date(date).getDate()} ${date.toLocaleTimeString()}`;
+  },
+  net_amount: (price) => {
+    (price) ? price = price - (Math.round(price * 0.1 * 100) / 100).toFixed(2): 0; 
+    return (Math.round(price * 100) / 100).toFixed(2);
+  },
+  gst_amount: (price) => {
+    (price) ? price = price * 0.1 : 0; 
+    return (Math.round(price * 100) / 100).toFixed(2);
+  },
+  round: (price) => {
+    return (price) ? (Math.round(price * 100) / 100).toFixed(2) : 0; 
+  }
+};
